b18ad3ee4f0a84497575b7c681088ba1b0ad41c4
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-13 15:17:37 +0800

    add layout for page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    // alias of $lib goes to src
+    // import Header from "$lib/components/Header.svelte"
+    import "$lib/styles/style.css"
+    import "$lib/styles/style.scss"
+</script>
+
+<!-- <Header/> -->
+<!-- <header>This is the Header</header> -->
+
+<main>
+    <slot />
+</main>
+
+<!-- <footer>This is the Footer</footer> -->
