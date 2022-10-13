e0321409cb69dc2e8431b397711a6a85f6dfb1cc
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-13 15:16:46 +0800

    add Header components, but still not used for now

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,39 @@
+<header>
+    <a href="/">Home</a>
+    <nav>
+        <ul>
+            <li>
+                <a href="/blog">Blog</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+            <li>
+                <a href="/contact">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    header { 
+        margin: 4rem;
+        max-width: 40rem auto;
+        border-color: black;
+        padding: 1rem;
+        background: slategrey;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+    ul { 
+        margin: 0;
+        list-style-type: none;
+        display: flex;
+        gap: 1rem;
+    }
+    a { 
+        text-decoration: none;
+        color: inherit;
+    }
+</style>
