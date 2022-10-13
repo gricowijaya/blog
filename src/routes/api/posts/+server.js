c7d17db134cc0c76860a47e58d0ddf65504b398d
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-13 18:01:29 +0800

    add function for endpoint

---
+++ src/routes/api/posts/+server.js
@@ -0,0 +1,16 @@
+import { fetchMarkdownPosts } from '$lib/utils'
+import { json } from '@sveltejs/kit'
+
+export const GET = async () => {
+  try { 
+    const allPosts = await fetchMarkdownPosts();
+
+    const sortedPosts = allPosts.sort((a, b) => {
+      return new Date(b.meta.date) - new Date(a.meta.date)
+    });
+
+    return json(sortedPosts);
+  } catch(err) { 
+    console.log(err);
+  }
+}
