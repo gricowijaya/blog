2a711702dc3a1b70ba52e8199a3d613d699f7913
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-13 18:02:27 +0800

    add slug for formatting the page

---
+++ src/routes/blog/posts/[slug]/+page.js
@@ -0,0 +1,15 @@
+export async function load({ params }) {
+    try { 
+        const post = await import (`../${params.slug}.md`);
+        const { title, date } = post.metadata
+        const content = post.default
+
+        return {
+            content,
+            title,
+            date,
+        }
+    } catch(err) { 
+       return new Error(`there's a problem -> ${err}`); 
+    }
+}
