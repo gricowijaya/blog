c24d79ddc143fd3a49a36c970527a1bfe874eacf
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-13 15:17:11 +0800

    add a few blogs

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import adapter from '@sveltejs/adapter-auto';
+import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess.js';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
+	kit: {
+		adapter: adapter()
+	},
+
+	preprocess: [
+		mdsvex(mdsvexConfig),
+		sveltePreprocess()
+	]
+};
+
+export default config;
