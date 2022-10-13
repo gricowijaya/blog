8b9c5a13ebbaa64f8ecdfacfbad6dc3eef98c9f4
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-13 18:02:03 +0800

    static navigation

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Contact</h1>
-<p>Here's my <a href="https://github.com/gricowijaya">Github</a></p>
+<h1>Here's the list of posts</h1>
+<!-- <p>Here's my <a href="https://github.com/gricowijaya">Github</a></p> -->
+
+<ul>
+    <li>
+        <a href="/blog/posts/network-operating-system-role">Network Operating System Role</a>
+    </li>
+    <li>
+        <a href="/blog/posts/NOS-Intro">NOS INTRO</a>
+    </li>
+    <li>
+        <a href="/blog/posts/linux-kernel">Linux Kernel</a>
+    </li>
+    <li>
+        <a href="/blog/posts/linux-privileges">Linux Privileges</a>
+    </li>
+    <li>
+        <a href="/blog/posts/linux-process-id">Linux Process</a>
+    </li>
+    <li>
+        <a href="/blog/posts/linux-scheduling">Linux Scheduling Part I</a>
+    </li>
+    <li>
+        <a href="/blog/posts/linux-scheduling-ii">Linux Scheduling Part II</a>
+    </li>
+    <li>
+        <a href="/blog/posts/sdn">Software Define Network</a>
+    </li>
+    <li>
+        <a href="/blog/posts/security-in-network-os">Software In Network Operating System</a>
+    </li>
+    <li>
+        <a href="/blog/posts/using-svelte">Using Svelte</a>
+    </li>
+</ul>
