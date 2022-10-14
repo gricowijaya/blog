6a2f76f1952a951b1863bd96856910ca1c6a1868
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-14 11:24:57 +0800

    add menu navbar

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,5 @@
+<nav class="hidden text-sice-40 text-gray-500 uppercase text-bold sm:block">
+  <a href="/" class="hover:text-gray-700 hover:no-underline">Home</a>
+  <a href="/blog" class="hover:text-gray-700 hover:no-underline">Blog</a>
+  <a href="/contact" class="hover:text-gray-700 hover:no-underline">Contact</a>
+</nav>
