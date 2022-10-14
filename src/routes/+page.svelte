111418561ba5a0f6681b5b8ab716df0ac8f52793
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-14 11:25:53 +0800

    create the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Contact</h1>
-<p>Here's my <a href="https://github.com/gricowijaya">Github</a></p>
+<style>
+    p { 
+        text-align: justify;
+    }
+</style>
+
+<h1>
+Here's my Home Page
+</h1>
+
+<div>
+<p>
+    This page is created by using <a href="https://svelte.dev/">Svelte</a> framework.
+    This is a very simple page which just renders my writing into a simple page.
+    It's just better for me.<br>
+</p>
+</div>
+
+<div class="learning-journey">
+    <p>
+        Currently I am on my way in learning backend programming with Node Js and Go. <br>
+    </p>
+    <p>
+        I also learning about DevOps, cause it's just fun. I use the thinkpad X1 Carbon for now to do my daily basis work. 
+    </p>
+</div>
+        
+<div class="operating-system">
+    <p>
+        The Operating System I just like the GNU/Linux OS, I've tried MacOS and Microsoft Windows before but it just doesn't work for me, i mean the tiling window manager, unbloated program (Well if u use arch is just a base program), and the main thing about linux that is free, But there's downside using linux, You can't use Microsoft Office. Still, it is the best OS I've ever tried. <br>
+    </p>
+</div>
